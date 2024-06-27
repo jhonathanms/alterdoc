37d5462d806517b4f4688e76589462e536d23333
jhonathan.dsn.shop <jhonathan.dsn.shop@alterdata.com.br>
2024-06-27 19:08:47 -0300

    feat: ajustado exibição de cards por tipo

--- src/components/editor/body/BodyCmp.vue
+++ src/components/editor/body/BodyCmp.vue
@@ -5,8 +5,8 @@
         <template v-for="(item, index) in store.projeto.conteudo" :key="index">
           <!-- Criar um construtor de cards com base no tipo. -->
           <CardItemCmp
-            :titulo="(item as IParagrafo).titulo "
-            :subtitulo="(item as IParagrafo).texto"
+            :titulo="getDados(item).titulo"
+            :subtitulo="getDados(item).subTitulo"
             :tipo="item.tipoConteudo"
             v-if="isParagrafo(item)"
           />
@@ -24,17 +24,42 @@
 </template>
 
 <script setup lang="ts">
-import type { IConteudo, IGrupo, IParagrafo } from '@/model/IBlueprint';
-import { useStoreBase } from '@/stores/storeBase';
-import AcoesCmp from './painelHome/acoes/AcoesCmp.vue';
-import CardItemCmp from './painelHome/card/CardItemCmp.vue';
-import PreviewCodigoCmp from './painelPreview/PreviewCodigoCmp.vue';
-import PreviewHtmlCmp from './painelPreview/PreviewHtmlCmp.vue';
+import type { IConteudo, IParagrafo } from '@/model/IBlueprint'
+import { useStoreBase } from '@/stores/storeBase'
+import AcoesCmp from './painelHome/acoes/AcoesCmp.vue'
+import CardItemCmp from './painelHome/card/CardItemCmp.vue'
+import PreviewCodigoCmp from './painelPreview/PreviewCodigoCmp.vue'
+import PreviewHtmlCmp from './painelPreview/PreviewHtmlCmp.vue'
 
 const store = useStoreBase()
 
 const isParagrafo = (item: IConteudo) => {
   return item && item.tipoConteudo !== 'configuracao' && (item as IParagrafo).nivel !== 0
+}
+
+const getDados = (item: IConteudo): { titulo: string; subTitulo: string } => {
+  switch (item.tipoConteudo) {
+    case 'paragrafo':
+    case 'grupo': {
+      const titulo = (item as IParagrafo).titulo || ''
+      const subTitulo = (item as IParagrafo).texto || ''
+      return { titulo, subTitulo }
+    }
+
+    case 'alerta':
+    case 'notacao': {
+      if (item.componentes?.length) {
+        const paragrafo: IParagrafo = item?.componentes[0] as IParagrafo
+        const titulo = paragrafo.titulo || ''
+        const subTitulo = paragrafo.texto || ''
+        return { titulo, subTitulo }
+      }
+      return { titulo: '', subTitulo: '' }
+    }
+
+    default:
+      return { titulo: '', subTitulo: '' }
+  }
 }
 </script>
 
